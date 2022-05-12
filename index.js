e3c8546fcee22110d361b0e54ba81b4eead1dd1d
Fábio Cristóvão <fabio.ascristovao@gmail.com>
2022-05-12 21:51:52 +0100

    starting layout

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-"hello"
+const container = document.getElementById("grid-container");
+
+function makeRows(rows, cols) {
+   container.style.setProperty('--grid-rows', rows);
+   container.style.setProperty('--grid-cols', cols);
+   for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      container.appendChild(cell).className = "grid-item";
+   };
+}; bhhfsddgjkikugddvhj
+
+makeRows(16, 16);
