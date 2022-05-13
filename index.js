60049711b846c3a765afe55cef3876e11f8b45ac
Fábio Cristóvão <fabio.ascristovao@gmail.com>
2022-05-13 16:59:17 +0100

    grid size adjustable mouveover working

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const container = document.getElementById("grid-container");
+const pixelRange = document.getElementById("pixel-range")
+const rangeLabel = document.getElementById("pixel-range-label")
+let gridNum = pixelRange.value;
 
-function makeRows(rows, cols) {
-   container.style.setProperty('--grid-rows', rows);
-   container.style.setProperty('--grid-cols', cols);
-   for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "grid-item";
-   };
-}; bhhfsddgjkikugddvhj
+function fillCell(e) {
+   e.target.style.backgroundColor = "red";
+}
 
-makeRows(16, 16);
+pixelRange.addEventListener("change", changeRange, false);
+
+function changeRange(e) {
+   gridNum = e.target.value;
+   rangeLabel.textContent = `Pixel range: ${gridNum}`;
+   buildGrid();
+}
+
+function clearGrid() {
+   while (container.firstChild) {
+      container.removeChild(container.lastChild);
+   }
+}
+
+function createCell() {
+   let cell = document.createElement("div");
+   cell.classList.add("grid-item");
+   cell.addEventListener("mouseover", fillCell, false);
+
+   return cell
+}
+
+function fillGrid() {
+   for (let i = 0; i < gridNum * gridNum; i++) {
+      container.appendChild(createCell());
+   }
+}
+
+function buildGrid() {
+   clearGrid();
+   container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+   container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+   fillGrid();
+};
+
+buildGrid();
+
+
+
+
+
+
+
